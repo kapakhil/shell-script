ad2cc24c6e0bbfb3836c270075ee96ec7aeb7782
adramalli <adra@ehrenfeldtech.com>
2024-03-06 22:55:29 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=akhil
+PERSON2=ab
+
+echo "$PERSON1: Hello $PERSON2 , how u doing"
+echo "$PERSON2: great what about u $PERSON1"
+echo "$PERSON1: what are u doing now $PERSON2"
+echo "$PERSON2: ntg $PERSON1"S
