737d4223cc8df94103708ba623abd08729deed72
adramalli <adra@ehrenfeldtech.com>
2024-03-12 13:56:36 +0530

    03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 : how are u $person2"
+echo "$person2 : fine, wt about u $person1"
+echo "$person1 : good, where are u now $person2"
+echo "$person2 : am settled in usa $person1"
