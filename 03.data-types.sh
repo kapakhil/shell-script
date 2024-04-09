ff687bc87f52fc809fff25dac4cb1efb54e7bc7a
kapakhil <kapakhil17@gmail.com>
2024-04-09 17:41:32 +0530

    shell data types

---
+++ 03.data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hi how are u $PERSON2"
+echo "$PERSON2 :: am doing great how about u $PERSON2"
+echo "$PERSON1 :: great to hear that $PERSON2"
+echo "$PERSON2 :: how its going $PERSON1 "
