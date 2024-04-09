d66af207bf987e13f7232cdbb4e52a97d349c831
kapakhil <kapakhil17@gmail.com>
2024-04-09 18:14:14 +0530

    data types

---
+++ 06.Arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("guava's", "papaya", "pineapple")
+
+echo "Before going out drink $FRUITS[2]"
+echo "Then eat $FRUITS[0] after that play for sometime "
+echo "Have $FRUITS[1] together, to get blood and energy"
+
+
+echo "IF U KNOW...U KNOW"
+
