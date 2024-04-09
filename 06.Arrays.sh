914c1f9f0ad36b72c9ccaef222d867e666cdcaf2
kapakhil <kapakhil17@gmail.com>
2024-04-09 18:28:44 +0530

    data types

--- 06.Arrays.sh
+++ 06.Arrays.sh
@@ -2,10 +2,10 @@
 
 FRUITS=("guava", "papaya", "pineapple")
 
-echo "Before going out drink $FRUITS{[2]}"
-echo "Then eat $FRUITS[0] after that play for sometime "
-echo "Have $FRUITS[1] together, to get blood and energy"
+echo "Before going out drink ${FRUITS[2]} or pineapple juice"
+echo "Then eat ${FRUITS[0]} after that play for sometime "
+echo "Have ${FRUITS[1]} together, to get energy and soo on"
 
 
-echo "IF U KNOW...U KNOW"
+echo "IF U KNOW...U KNOW   :)  "
 
