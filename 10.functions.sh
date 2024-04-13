89da318fe26244e951bae8dcf9b894fc3bec345a
kapakhil <kapakhil17@gmail.com>
2024-04-13 19:47:14 +0530

    functions

--- 10.functions.sh
+++ 10.functions.sh
@@ -2,20 +2,19 @@
 
 ID=(id -u)
 AKHLI(){
-    if [ $1 -ne 0 ]
+if [ $1 -ne 0 ]
 then
-echo "ERROR : $2.....failed"
+    echo "ERROR : $2.....failed"
 else
-echo "$2.......success"
+    echo "$2.......success"
 fi
 }
 
-ID=( id -u 0)
 if [ $ID -ne 0 ]
 then 
-echo "ERROR ::- you can as a root user"
+    echo "ERROR ::- you can as a root user"
 else
-echo "you r the root user u can run"
+    echo "you r the root user u can run"
 fi
 
 yum install mariadb -y
