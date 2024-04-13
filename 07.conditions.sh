fc68c6a83233809ab269057e2f8b877741be33eb
kapakhil <kapakhil17@gmail.com>
2024-04-13 18:11:51 +0530

    conditions

--- 07.conditions.sh
+++ 07.conditions.sh
@@ -5,5 +5,5 @@ if [ $NUMBER -gt 100 ]
 then 
     echo "Given number is $NUMBER greater than 100"
 else
-    echo "Given number is not grater than 100"
+    echo "Given number is $NUMBER not grater than 100"
 fi 
