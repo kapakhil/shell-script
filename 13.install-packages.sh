892e9854497df5256cd8718555aa48de26d7b674
kapakhil <kapakhil17@gmail.com>
2024-05-25 21:13:34 +0530

    install packages

---
+++ 13.install-packages.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+  if [ $1 - ne 0 ]
+   then 
+    echo "$2 ....$R FAILED $N"
+   else
+    echo "$2 ....$G SUCCESS $N"
+   fi 
+}
+
+if [ $ID - ne 0 ]
+ then 
+  echo "$R ERROR ::- get the sudo access and run script $N"
+else 
+  echo " $G you are the root user $N"
+fi  
+
+for package in $@
+ do 
+    yum list install $package
+    if ( $? - ne 0)
+       then
+           yum install $package -y 
+           VALIDATE $? "Installation of $package"
+    else
+        echo "$package is already installed...$Y SKIPPING $N "
+    fi        
+ done
+
